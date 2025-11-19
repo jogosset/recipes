f0d787424644e8c030d2697785ca3b0672b8023b
Jon Gossett <63742516+jogosset@users.noreply.github.com>
2025-11-18 23:25:56 -0600

    Create recipes.js

---
+++ recipes.js
@@ -0,0 +1,15 @@
+const RECIPES = [
+  {
+    "title": "Margarita",
+    "ingredients": [
+      "2 oz Tequila",
+      "1 oz Agave Syrup",
+      "1 oz Lime Juice"
+    ],
+    "instructions": [
+      "Shake and strain over ice."
+    ],
+    "garnish": "Salted rim"
+  },
+  // ...rest of your recipes
+];
