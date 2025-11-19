c68da714ff7afa44d2fcfd6ea0b882a32745ef6a
Jon Gossett <63742516+jogosset@users.noreply.github.com>
2025-11-18 23:39:57 -0600

    Update recipes.js

--- recipes.js
+++ recipes.js
@@ -1,4 +1,34 @@
-const RECIPES = [
+[
+  {
+    "title": "FUN DRINKS   (40)",
+    "ingredients": [
+      "1.5 oz Spiced Rum",
+      "1.5 oz Coconut Rum",
+      "1 oz Blue Curacao",
+      "6 oz Pineapple Juice",
+      ".5 oz Lime Juice",
+      "1 oz Vodka",
+      "1 oz Rum",
+      ".5 oz Blue Curacao",
+      "4 oz Lemonade",
+      ".5 oz Peach Schnapps",
+      "1 oz Fireball",
+      "1 oz Amaretto",
+      "2 oz Liquor 43",
+      "3 oz Orange Juice",
+      "2 oz Heavy Cream",
+      "1.5 oz Coconut Rum/Vodka"
+      // ...more ingredients
+    ],
+    "instructions": [
+      "splash of Pineapple Juice",
+      "Top with Dr. Pepper",
+      "Orange Creamsicle (shake n strain)",
+      "Splash of Ginger Ale"
+      // ...more steps
+    ],
+    "garnish": ""
+  },
   {
     "title": "Margarita",
     "ingredients": [
@@ -10,6 +40,6 @@ const RECIPES = [
       "Shake and strain over ice."
     ],
     "garnish": "Salted rim"
-  },
-  // ...rest of your recipes
-];
+  }
+  // ...more recipes
+]
